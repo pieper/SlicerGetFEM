54e02c482542bc75fc35a89cec7ddaa896a19e12
Steve Pieper <pieper@isomics.com>
2023-08-19 15:09:41 -0400

    Add tripod.py experiment notes

--- Experiments/tripod.py
+++ Experiments/tripod.py
@@ -1,1 +1,19 @@
+"""
+1) install getfem
+`sudo apt-get install python3-getfem++`
+2) checkout demos
+`git clone https://github.com/getfem-doc/getfem.git`
+3) run demo
+```
+cd getfem/interface/python
+python3 demo_tripod.py
+```
+4) run the following in Slicer
+"""
+
+m = loadModel("tripod.vtk")
+pos = arrayFromModelPoints(m)
+disp = arrayFromModelPointData(m, "Displacement")
+pos[:] = pos + disp
+arrayFromModelPointsModified(m)
 
